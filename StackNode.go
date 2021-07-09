42db7afbc0be14de139746685eda12105340fea1
Avtandil Chachanidze <83176152+Chachanidze29@users.noreply.github.com>
2021-07-09 17:09:33 +0400

    Add files via upload

---
+++ StackNode.go
@@ -0,0 +1,47 @@
+package main
+
+import "fmt"
+
+type Node struct {
+	value int
+	next  *Node
+}
+
+type Stack struct {
+	size int
+	top  *Node
+}
+
+func (s *Stack) len() int {
+	return s.size
+}
+
+func (s *Stack) push(val int) {
+	s.size++
+	s.top = &Node{
+		value: val,
+		next:  s.top,
+	}
+}
+
+func (s *Stack) pop() int {
+	if s.len() == 0 {
+		panic("Stack is empty")
+	}
+	s.size--
+	tmp := s.top.value
+	s.top = s.top.next
+	return tmp
+}
+
+func (s *Stack) printInfo() {
+	curr := s.top
+	for i := 0; i < s.size; i++ {
+		fmt.Println(curr.value)
+		curr = curr.next
+	}
+}
+
+func main() {
+
+}
