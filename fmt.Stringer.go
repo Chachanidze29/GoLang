8ad5a7f02a3f6a73aea56f429f5e6b0bae1dc0ee
Avtandil Chachanidze <83176152+Chachanidze29@users.noreply.github.com>
2021-08-06 09:50:15 +0400

    Add files via upload

---
+++ fmt.Stringer.go
@@ -0,0 +1,34 @@
+package main
+
+import "fmt"
+
+type Person struct {
+	Name string
+	Age  int
+}
+
+func (p Person) String() string {
+	return fmt.Sprintf("%v (%v years)", p.Name, p.Age)
+}
+
+type Developer struct {
+	Person
+	progLang string
+}
+
+func (d Developer) String() string {
+	return d.Person.String() + fmt.Sprintf("Programming language %v", d.progLang)
+}
+
+func main() {
+	a := Person{"Arthur Dent", 42}
+	z := Person{"Zaphod Beeblebrox", 9001}
+	fmt.Println(a, z)
+	d1 := Developer{Person{"Avtandil chachanidze", 18}, "Go"}
+	d2 := Developer{Person{"Vakho misuradze", 28}, "JS"}
+	fmt.Println(d1, d2)
+	arr := []fmt.Stringer{&a, &z, &d1, &d2}
+	for _, v := range arr {
+		fmt.Println(v)
+	}
+}
