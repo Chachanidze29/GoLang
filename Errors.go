3a1ed7654dc083d023ad207850d1c5e2aad6c9a2
Avtandil Chachanidze <83176152+Chachanidze29@users.noreply.github.com>
2021-08-06 11:42:40 +0400

    Update Errors.go

--- Errors.go
+++ Errors.go
@@ -2,11 +2,37 @@ package main
 
 import (
 	"fmt"
-	"image"
 	"log"
 )
 
+type ErrNegativeSqrt float64
+
+func (e ErrNegativeSqrt) Error() string {
+	return fmt.Sprint("cannot Sqrt negative number: ", float64(e))
+}
+
+func Sqrt(x float64) (float64, error) {
+	if x < 0 {
+		return 0, ErrNegativeSqrt(-2)
+	}
+	z := x
+	for i := 0; i < 10; i++ {
+		z -= (z*z - x) / (2 * z)
+	}
+	return z, nil
+}
+
 func main() {
-	fmt.Println(image.Black)
-	log.Prefix()
+	log.SetPrefix("Message:")
+	log.SetFlags(0)
+	num1, err := Sqrt(2)
+	if err != nil {
+		fmt.Println(err)
+	}
+	fmt.Println(num1)
+	num2, err := Sqrt(-2)
+	if err != nil {
+		log.Fatal(err) // Log.Fatal() calls overloaded Error() Method
+	}
+	fmt.Println(num2)
 }
